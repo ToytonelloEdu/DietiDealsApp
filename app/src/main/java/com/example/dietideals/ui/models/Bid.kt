c0d54c8ffeb75334280e186bc8f0a85ee7820808
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-18 01:46:26 +0200

    Added optional fields to Bid

--- app/src/main/java/com/example/dietideals/ui/models/Bid.kt
+++ app/src/main/java/com/example/dietideals/ui/models/Bid.kt
@@ -12,7 +12,7 @@ data class Bid(
     val time: Timestamp? = null
 ) {
     constructor(netBid: NetBid) : this(
-        netBid.auction.let {if (it != null) Auction(it) else null},
+        netBid.auction.let { it?.toAuction() },
         netBid.buyer.let {if (it != null) Buyer(it) else null},
         netBid.bidder,
         netBid.amount,
@@ -20,6 +20,7 @@ data class Bid(
             netBid.time
                 ?.replace("Z[UTC]", "")
                 ?.replaceFirst("T", " ")
+                ?: "2000-01-01 00:00:00"
         )
     )
 }
