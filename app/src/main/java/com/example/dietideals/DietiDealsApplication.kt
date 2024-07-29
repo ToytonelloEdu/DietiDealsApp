af1db7552f76c768a65723f53ababe039b849356
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-30 01:15:16 +0200

    Added new Container for Offline logic

--- app/src/main/java/com/example/dietideals/DietiDealsApplication.kt
+++ app/src/main/java/com/example/dietideals/DietiDealsApplication.kt
@@ -3,11 +3,14 @@ package com.example.dietideals
 import android.app.Application
 import com.example.dietideals.data.AppContainer
 import com.example.dietideals.data.RetrofitAppContainer
+import com.example.dietideals.data.RoomAppContainer
 
 class DietiDealsApplication : Application() {
     lateinit var container: AppContainer
+    lateinit var offlineContainer: AppContainer
     override fun onCreate() {
         super.onCreate()
         container = RetrofitAppContainer()
+        offlineContainer = RoomAppContainer(this)
     }
 }
