6cbc1db0f2513398d3d1e8f8559f457900d9bdb3
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-30 01:13:59 +0200

    Migrated to Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,5 @@ buildscript {
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
