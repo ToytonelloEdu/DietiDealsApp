872442d9c9e4d1585d975a844d32b0f1f2b594d4
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-07-31 01:12:31 +0200

    Added state for NewAuction view

--- app/src/main/java/com/example/dietideals/data/AppUiState.kt
+++ app/src/main/java/com/example/dietideals/data/AppUiState.kt
@@ -1,8 +1,10 @@
 package com.example.dietideals.data
 
+import com.example.dietideals.domain.auxiliary.NewAuction
 import com.example.dietideals.domain.auxiliary.NewUser
 import com.example.dietideals.ui.AuctionFetchState
 import com.example.dietideals.ui.HomeFetchState
+import com.example.dietideals.ui.NewAuctionState
 import com.example.dietideals.ui.SignUpState
 import com.example.dietideals.ui.UserState
 
@@ -10,5 +12,6 @@ data class AppUiState(
     val userState: UserState = UserState.NotLoggedIn(),
     val currentHomeState: HomeFetchState = HomeFetchState.Loading,
     val currentAuctionState: AuctionFetchState = AuctionFetchState.Loading,
-    val signUpState: SignUpState = SignUpState.Initial(NewUser())
+    val signUpState: SignUpState = SignUpState.Initial(NewUser()),
+    val newAuctionState: NewAuctionState = NewAuctionState.Initial(NewAuction())
 )
