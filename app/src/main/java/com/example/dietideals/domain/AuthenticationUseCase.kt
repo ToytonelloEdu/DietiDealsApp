37d82b442d30b03a5317897a03d3941aacd5546a
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-08-21 12:48:11 +0200

    Added signUp method

--- app/src/main/java/com/example/dietideals/domain/AuthenticationUseCase.kt
+++ app/src/main/java/com/example/dietideals/domain/AuthenticationUseCase.kt
@@ -1,11 +1,21 @@
 package com.example.dietideals.domain
 
+import android.util.Log
 import com.example.dietideals.data.repos.AuthRepository
 import com.example.dietideals.data.repos.UsersRepository
+import com.example.dietideals.domain.auxiliary.NewUser
 
 class AuthenticationUseCase (
     private val authRepo : AuthRepository,
     private val userRepo : UsersRepository
 ) {
+    suspend fun signUp(newUser: NewUser) {
+        try {
+            val netUser = authRepo.signup(newUser.toUser())
+        } catch (e: Exception) {
+            Log.e("AppViewModel", "Error: ${e.message}")
+        }
+
+    }
 
 }
