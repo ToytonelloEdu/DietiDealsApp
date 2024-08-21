dcf2951f47dd8f13b0bda07d6e828037f4763382
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-08-21 12:43:36 +0200

    Create ImageUploadUseCase.kt

---
+++ app/src/main/java/com/example/dietideals/domain/ImageUploadUseCase.kt
@@ -0,0 +1,52 @@
+package com.example.dietideals.domain
+
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import com.example.dietideals.data.repos.ImagesRepository
+import java.io.File
+import java.io.FileInputStream
+
+class ImageUploadUseCase(
+    private val imagesRepository: ImagesRepository
+) {
+    suspend fun uploadProfileImage(context: Context, username: String, uri: Uri) {
+        val parcelFileDescriptor = context.contentResolver.openFileDescriptor(
+            uri, "r", null
+        ) ?: throw IllegalArgumentException("Invalid file descriptor")
+
+        val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
+        val file = File(context.cacheDir, context.contentResolver.getFileName(uri))
+        val outputStream = file.outputStream()
+        inputStream.copyTo(outputStream)
+
+        imagesRepository.uploadProfileImage(username, file)
+
+        parcelFileDescriptor.close()
+    }
+
+    suspend fun uploadAuctionImage(context: Context, auctionId: Int, imageIndex: Int, uri: Uri) {
+        val parcelFileDescriptor = context.contentResolver.openFileDescriptor(
+            uri, "r", null
+        ) ?: throw IllegalArgumentException("Invalid file descriptor")
+
+        val inputStream = FileInputStream(parcelFileDescriptor.fileDescriptor)
+        val file = File(context.cacheDir, context.contentResolver.getFileName(uri))
+        val outputStream = file.outputStream()
+        inputStream.copyTo(outputStream)
+
+        imagesRepository.uploadAuctionImage(auctionId, imageIndex, file)
+
+        parcelFileDescriptor.close()
+    }
+}
+
+private fun ContentResolver.getFileName(uri: Uri): String {
+    var name = ""
+    val cursor = query(uri, null, null, null, null)
+    cursor?.use {
+        it.moveToFirst()
+        name = it.getString(it.getColumnIndexOrThrow("_display_name"))
+    }
+    return name
+}
