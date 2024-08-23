e7388f4406dae1555a84f10d6de201e7779f9043
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-08-23 17:24:12 +0200

    Refactored code to UseCase classes

--- app/src/main/java/com/example/dietideals/ui/AppViewModel.kt
+++ app/src/main/java/com/example/dietideals/ui/AppViewModel.kt
@@ -10,12 +10,10 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.dietideals.DietiDealsApplication
 import com.example.dietideals.data.AppUiState
-import com.example.dietideals.data.repos.AuctionsRepository
-import com.example.dietideals.data.repos.AuthRepository
 import com.example.dietideals.data.repos.BidsRepository
-import com.example.dietideals.data.repos.TagsRepository
-import com.example.dietideals.data.repos.UsersRepository
+import com.example.dietideals.domain.AuctioneerUseCase
 import com.example.dietideals.domain.AuthenticationUseCase
+import com.example.dietideals.domain.BuyerUseCase
 import com.example.dietideals.domain.HomePageUseCase
 import com.example.dietideals.domain.ImageUploadUseCase
 import com.example.dietideals.domain.auxiliary.NewAuction
@@ -29,128 +27,66 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import retrofit2.HttpException
+import kotlinx.coroutines.runBlocking
 import java.sql.Timestamp
-import java.util.Date
 
 
 class AppViewModel(
     private val homePageUseCase: HomePageUseCase,
     private val authenticationUseCase: AuthenticationUseCase,
+    private val auctioneerUseCase: AuctioneerUseCase,
+    private val buyerUseCase: BuyerUseCase,
     private val imageUploadUseCase: ImageUploadUseCase,
-    private val auctionsRepository: AuctionsRepository,
-    private val offlineAuctionsRepository: AuctionsRepository,
-    private val usersRepository: UsersRepository,
-    private val authRepository: AuthRepository,
-    private val tagsRepository: TagsRepository,
     private val bidsRepository: BidsRepository,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(AppUiState())
     val uiState: StateFlow<AppUiState> = _uiState.asStateFlow()
 
-    private lateinit var _auctions: List<Auction>
     private val auctions: List<Auction>
         get() {
-            return (_uiState.value.currentHomeState as? HomeFetchState.HomeSuccess)?.auctions ?: emptyList()
+            return (_uiState.value.currentHomeState.getAuctionsOrNull()) ?: emptyList()
         }
 
     init {
-        //onLoginClicked("ciroanastasio", "forzanapoli")
+        runBlocking {
+            authenticationUseCase.rememberLogIn(_uiState)
+            viewModelScope.launch {
+                authenticationUseCase.checkRemember(_uiState)
+                refreshAuctionsOrBids()
+            }
+        }
         serverHomepageAuctions()
+    }
+
+    private fun refreshAuctionsOrBids() {
+        when (AuthenticationUseCase.user) {
+            is Auctioneer -> refreshUserAuctions()
+            is Buyer -> refreshUserBids()
+        }
     }
 
     private fun serverHomepageAuctions() {
         viewModelScope.launch {
-            _uiState.update { currentState ->
-                try {
-                    _auctions = auctionsRepository.getAuctions()
-                    Log.i("AppViewModel", "Success")
-                    currentState.copy(
-                        currentHomeState = HomeFetchState.HomeSuccess(_auctions)
-                    )
+                if(homePageUseCase.getHomePageAuctions(_uiState)){
+                    launch {
+                        homePageUseCase.saveAuctionToDb(auctions)
+                    }
                 }
-                catch (e: Exception) {
-                    _auctions = emptyList()
-                    Log.e("AppViewModel", "Error:$e-> ${e.message}")
-                    currentState.copy(
-                        currentHomeState = HomeFetchState.Error("Error: ${e.message}")
-                    )
-                }
-            }
         }
     }
 
     fun onAuctionClicked(passedAuction: Auction, isDirectBid: Boolean) {
         _uiState.update { currentState ->
-            currentState.copy(
-                currentAuctionState = AuctionFetchState.AuctionSuccess(passedAuction),
-            )
+            currentState.copy(currentAuctionState = AuctionFetchState.AuctionSuccess(passedAuction))
         }
         viewModelScope.launch {
-            _uiState.update { currentState ->
-                try {
-                    val auction = auctionsRepository.getAuctionById(passedAuction.id!!)
-                    currentState.copy(
-                        currentAuctionState = AuctionFetchState.AuctionSuccess(auction, auction.bids)
-                    )
-                } catch (e: Exception) {
-                    Log.e("AppViewModel", "Error: ${e.message}")
-                    currentState.copy(
-                        currentAuctionState = AuctionFetchState.Error("Error: ${e.message}")
-                    )
-                }
-            }
+            homePageUseCase.getAuctionDetails(_uiState, passedAuction)
         }
     }
 
     fun onLoginClicked(handle: String, password: String) {
         viewModelScope.launch {
-            try {
-                authRepository.auth(handle, password).let {
-                        updateUserStateByHandle(handle)
-                        Log.i("AppViewModel", "Response: $it")
-                }
-            } catch (e: HttpException) {
-                Log.e("AppViewModel", "Error: ${e.message}")
-                if(e.code() == 401) {
-                    _uiState.update { currentState ->
-                        currentState.copy(
-                            userState = UserState.NotLoggedIn(true)
-                        )
-                    }
-                }
-            } catch (e: Exception) {
-                Log.e("AppViewModel", "Error: ${e.message}")
-            }
-        }
-    }
-
-    private suspend fun updateUserStateByHandle(handle: String) {
-        val user = usersRepository.getUserByHandle(handle).let { netUser ->
-            when (netUser.userType) {
-                "Auctioneer" -> Auctioneer(netUser)
-                "Buyer" -> Buyer(netUser)
-                else -> throw IllegalArgumentException("NetUser type not found")
-            }
-        }
-        when (user) {
-            is Auctioneer -> {
-                _uiState.update { currentState ->
-                    val newAuction = currentState.newAuctionState.newAuction.apply{
-                        auctioneer = user
-                    }
-                    currentState.copy(
-                        userState = UserState.Vendor(user),
-                        newAuctionState = NewAuctionState.Initial(newAuction)
-                    )
-                }
-            }
-            is Buyer -> {
-                _uiState.update { currentState ->
-                    currentState.copy(userState = UserState.Bidder(user))
-                }
-            }
-            else -> throw IllegalArgumentException("NetUser type not found")
+            authenticationUseCase.logIn(_uiState, handle, password)
         }
     }
 
@@ -170,31 +106,11 @@ class AppViewModel(
         }
     }
 
-    fun retryHomePageLoading() {
-        _uiState.update { currentState ->
-            currentState.copy(
-                currentHomeState = HomeFetchState.Loading
-            )
-        }
-        serverHomepageAuctions()
-    }
-
     fun refreshHomePage() {
         viewModelScope.launch {
-            _uiState.update { currentState ->
-                currentState.copy(
-                    currentHomeState = HomeFetchState.HomeSuccess(auctions, true)
-                )
-            }
-            try{
-                _auctions = auctionsRepository.getAuctions()
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        currentHomeState = HomeFetchState.HomeSuccess(_auctions)
-                    )
-                }
-            } catch (e: Exception) {
-                Log.e("AppViewModel", "Error: ${e.message}")
+            _uiState.update { it.copy(currentHomeState = it.currentHomeState.toggleRefreshing()) }
+            if(homePageUseCase.refreshAuction(_uiState)) {
+                homePageUseCase.saveAuctionToDb(auctions)
             }
 
         }
@@ -202,60 +118,26 @@ class AppViewModel(
 
     fun onNewAuctionConfirm(newAuction: NewAuction, context: Context) {
         viewModelScope.launch {
-            newAuction.date = Date();
-            _uiState.update { currentState ->
-                currentState.copy(
-                    newAuctionState = NewAuctionState.Loading(newAuction)
-                )
-            }
-            try {
-
-                val auction = auctionsRepository.addAuction(newAuction.toAuction(), "")
-
-                newAuction.picturePaths.forEachIndexed { index, path ->
+            auctioneerUseCase.createNewAuction(_uiState, newAuction) { uris, auction ->
+                uris.forEachIndexed { index, path ->
                     imageUploadUseCase.uploadAuctionImage(context, auction.id!!, index, path)
-                }
-
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        newAuctionState = NewAuctionState.Success(newAuction)
-                    )
-                }
-
-                refreshUserAuctions()
-            } catch (e: NullPointerException) {
-                Log.e("AppViewModel", "Error: $e -> ${e.message}")
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        newAuctionState = NewAuctionState.Error(newAuction,"Beware, some fields were left empty!")
-                    )
-                }
-            } catch (e: Exception) {
-                Log.e("AppViewModel", "Error: ${e.message}")
-                _uiState.update { currentState ->
-                    currentState.copy(
-                        newAuctionState = NewAuctionState.Error(newAuction,"Error: $e -> ${e.message}")
-                    )
                 }
             }
         }
     }
 
-    private fun refreshUserAuctions() {
-        viewModelScope.launch {
-            val auctioneer = (uiState.value.userState as UserState.Vendor).auctioneer
-            _auctions = auctionsRepository.getAuctionsByUser(auctioneer.username)
-            _uiState.update { currentState ->
-                currentState.copy(
-                    userState = UserState.Vendor(auctioneer.copy(auctions = _auctions.toMutableList())),
-                )
-            }
+    fun refreshUserAuctions(swiped: Boolean = false) {
+        viewModelScope.launch(
+        ) {
+            if(swiped) _uiState.update { it.copy(isRefreshing = true) }
+
+            auctioneerUseCase.refreshAuctions(_uiState)
         }
     }
 
     fun onSignUpClicked(newUser: NewUser) {
         viewModelScope.launch {
-            authenticationUseCase.signUp(newUser)
+            authenticationUseCase.signUp(_uiState, newUser)
         }
     }
 
@@ -264,13 +146,13 @@ class AppViewModel(
             try {
                 val buyer = (uiState.value.userState as UserState.Bidder).buyer
                 val bid = Bid(
-                    auction,
-                    buyer,
-                    buyer.username,
-                    amount,
-                    Timestamp(System.currentTimeMillis())
+                    auction = auction,
+                    buyer = buyer,
+                    bidder = buyer.username,
+                    amount = amount,
+                    time = Timestamp(System.currentTimeMillis())
                 )
-                bidsRepository.postBid(bid, "")
+                bidsRepository.postBid(bid, AuthenticationUseCase.token)
                 refreshUserBids()
             } catch (e: Exception) {
                 Log.e("AppViewModel", "Error: $e -> ${e.printStackTrace()}")
@@ -278,22 +160,39 @@ class AppViewModel(
         }
     }
 
-    private fun refreshUserBids() {
+    fun refreshUserBids(swiped: Boolean = false) {
         viewModelScope.launch {
-            val buyer = (uiState.value.userState as UserState.Bidder).buyer
-            val bids = bidsRepository.getBidsByUser(buyer.username)
-            _uiState.update { currentState ->
-                currentState.copy(
-                    userState = UserState.Bidder(buyer.copy(bids = bids.toMutableList())),
-                )
+            if(swiped) _uiState.update { it.copy(isRefreshing = true) }
+            try {
+                val buyer = (uiState.value.userState as UserState.Bidder).buyer
+                val bids = bidsRepository.getBidsByUser(buyer.username)
+                _uiState.update { currentState ->
+                    currentState.copy(
+                        userState = UserState.Bidder(buyer.copy(bids = bids.toMutableList())),
+                        isRefreshing = false
+                    )
+                }
+            } catch (e: Exception) {
+                Log.e("AppViewModel", "Error: ${e.message}")
+                _uiState.update {
+                    it.copy(isRefreshing = false)
+                }
             }
         }
     }
 
     fun onLogoutClicked() {
+        viewModelScope.launch {
+            authenticationUseCase.logOut(_uiState)
+        }
+    }
+
+    fun onNewAuctionClicked() {
         _uiState.update { currentState ->
             currentState.copy(
-                userState = UserState.NotLoggedIn()
+                newAuctionState = NewAuctionState.Initial(
+                    NewAuction(auctioneer = AuthenticationUseCase.user as Auctioneer)
+                )
             )
         }
     }
@@ -306,19 +205,17 @@ class AppViewModel(
                 val auctionsRepository = application.container.auctionsRepository
                 val offlineAuctionsRepository = application.offlineContainer.auctionsRepository
                 val usersRepository = application.container.usersRepository
+                val offlineUsersRepository = application.offlineContainer.usersRepository
                 val authRepository = application.container.authRepository
                 val tagsRepository = application.container.tagsRepository
                 val imagesRepository = application.container.imagesRepository
                 val bidRepository = application.container.bidsRepository
                 AppViewModel(
                     homePageUseCase = HomePageUseCase(auctionsRepository, offlineAuctionsRepository),
-                    authenticationUseCase = AuthenticationUseCase(authRepository, usersRepository),
+                    authenticationUseCase = AuthenticationUseCase(authRepository, usersRepository, offlineUsersRepository),
+                    auctioneerUseCase = AuctioneerUseCase(auctionsRepository),
+                    buyerUseCase = BuyerUseCase(),
                     imageUploadUseCase = ImageUploadUseCase(imagesRepository),
-                    auctionsRepository = auctionsRepository,
-                    offlineAuctionsRepository = offlineAuctionsRepository,
-                    usersRepository = usersRepository,
-                    authRepository = authRepository,
-                    tagsRepository = tagsRepository,
                     bidsRepository = bidRepository
                 )
             }
