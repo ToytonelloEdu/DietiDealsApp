e15195035c2271c91c3ea59c34d5d5aa79567a2a
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-08-23 17:24:25 +0200

    Added utility methods

--- app/src/main/java/com/example/dietideals/ui/FetchStates.kt
+++ app/src/main/java/com/example/dietideals/ui/FetchStates.kt
@@ -6,22 +6,48 @@ import com.example.dietideals.domain.models.Auction
 import com.example.dietideals.domain.models.Auctioneer
 import com.example.dietideals.domain.models.Bid
 import com.example.dietideals.domain.models.Buyer
+import com.example.dietideals.domain.models.User
 
 
 sealed interface UserState {
     data class NotLoggedIn(val wrongCredentials: Boolean = false) : UserState
     data class Bidder(val buyer: Buyer) : UserState
     data class Vendor(val auctioneer: Auctioneer) : UserState
+
+    fun getUserOrNull(): User? {
+        return when(this){
+            is Vendor -> auctioneer
+            is Bidder -> buyer
+            is NotLoggedIn -> null
+        }
+    }
 }
 
 sealed interface HomeFetchState {
     data class HomeSuccess(val auctions: List<Auction>, val isRefreshing: Boolean = false) : HomeFetchState
     data object Loading : HomeFetchState
-    data class Error(val message: String? = null) : HomeFetchState
+    data class Error(val auctions: List<Auction>, val isRefreshing: Boolean = false, val message: String? = null) : HomeFetchState
+
+    fun getAuctionsOrNull(): List<Auction>? {
+        return when(this){
+            is HomeSuccess -> auctions
+            is Error -> auctions
+            is Loading -> null
+        }
+    }
+
+    fun toggleRefreshing(): HomeFetchState {
+        return when(this){
+            is HomeSuccess -> HomeSuccess(auctions, !isRefreshing)
+            is Error -> HomeSuccess(auctions, !isRefreshing)
+            is Loading -> this
+        }
+    }
+
 }
 
 sealed interface AuctionFetchState {
-    data class AuctionSuccess(val auction: Auction, val bids: List<Bid>? = null, val photos: List<String> = emptyList()) : AuctionFetchState
+    data class AuctionSuccess(val auction: Auction) : AuctionFetchState
     data object Loading : AuctionFetchState
     data class Error(val message: String? = null) : AuctionFetchState
 }
@@ -29,6 +55,7 @@ sealed interface AuctionFetchState {
 sealed class SignUpState (val newUser: NewUser) {
     class Initial(newUser: NewUser, val formInvalid: Boolean = false) : SignUpState(newUser)
     class Loading(newUser: NewUser) : SignUpState(newUser)
+    class Success(newUser: NewUser): SignUpState(newUser)
     class Error(newUser: NewUser, val message: String? = null) : SignUpState(newUser)
 }
 
