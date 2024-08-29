07d6c6d6ca5668e779c180732aab27983d189963
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-08-29 14:49:12 +0200

    Optimized imports

--- app/src/main/java/com/example/dietideals/ui/components/Overlays.kt
+++ app/src/main/java/com/example/dietideals/ui/components/Overlays.kt
@@ -1,6 +1,5 @@
 package com.example.dietideals.ui.components
 
-import android.app.Dialog
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -16,12 +15,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
-import com.example.dietideals.domain.models.Auction
 import com.example.dietideals.domain.models.SilentAuction
 import com.example.dietideals.domain.models.Tag
 import com.example.dietideals.ui.theme.DietiDealsTheme
