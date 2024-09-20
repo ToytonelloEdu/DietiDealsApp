383d65e8548f6c73a69dd3f641cbf9182663ab2d
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-09-20 12:14:32 +0200

    Changed UiState for Auction search use-case

--- app/src/main/java/com/example/dietideals/data/AppUiState.kt
+++ app/src/main/java/com/example/dietideals/data/AppUiState.kt
@@ -2,9 +2,11 @@ package com.example.dietideals.data
 
 import com.example.dietideals.domain.auxiliary.NewAuction
 import com.example.dietideals.domain.auxiliary.NewUser
+import com.example.dietideals.domain.auxiliary.SearchQuery
 import com.example.dietideals.ui.AuctionFetchState
 import com.example.dietideals.ui.HomeFetchState
 import com.example.dietideals.ui.NewAuctionState
+import com.example.dietideals.ui.SearchQueryState
 import com.example.dietideals.ui.SignUpState
 import com.example.dietideals.ui.UserState
 
@@ -14,7 +16,11 @@ data class AppUiState(
     val currentAuctionState: AuctionFetchState = AuctionFetchState.Loading,
     val signUpState: SignUpState = SignUpState.Initial(NewUser()),
     val newAuctionState: NewAuctionState = NewAuctionState.Initial(NewAuction()),
+    val searchQueryState: SearchQueryState = SearchQueryState.Initial(SearchQuery()),
 
+    val showSearchDialog: Boolean = false,
+    val showNotificationsDialog: Boolean = false,
+    val showAllAuctions: Boolean = false,
     val isRefreshing: Boolean = false,
     val isDirectBid: Boolean = false,
     val isOnline: Boolean = true
