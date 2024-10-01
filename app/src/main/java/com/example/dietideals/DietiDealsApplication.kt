341725dcbd24a04a84add211cce3a78affb8a8d5
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-10-01 10:36:13 +0200

    Added logic for WorkManager functionalities

--- app/src/main/java/com/example/dietideals/DietiDealsApplication.kt
+++ app/src/main/java/com/example/dietideals/DietiDealsApplication.kt
@@ -1,16 +1,36 @@
 package com.example.dietideals
 
 import android.app.Application
+import androidx.work.Configuration
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
 import com.example.dietideals.data.AppContainer
 import com.example.dietideals.data.RetrofitAppContainer
 import com.example.dietideals.data.RoomAppContainer
+import com.example.dietideals.domain.background.NotificationWorker
+import com.example.dietideals.domain.background.NotificationWorkerFactory
+import java.util.concurrent.TimeUnit
 
 class DietiDealsApplication : Application() {
+
     lateinit var container: AppContainer
     lateinit var offlineContainer: AppContainer
     override fun onCreate() {
         super.onCreate()
         container = RetrofitAppContainer(this)
         offlineContainer = RoomAppContainer(this)
+
+        WorkManager.initialize(
+            this,
+            Configuration.Builder()
+                .setWorkerFactory(NotificationWorkerFactory(
+                    container.notificationsRepository,
+                    offlineContainer.notificationsRepository
+                ))
+                .build()
+        )
+
+        val notifsFetchBuilder = PeriodicWorkRequestBuilder<NotificationWorker>(15, TimeUnit.MINUTES)
+        WorkManager.getInstance(this).enqueue(notifsFetchBuilder.build())
     }
 }
