3f12f4e34bd14d921c086423932d387f322ba248
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-10-01 10:32:12 +0200

    Added Links class for User socials

---
+++ app/src/main/java/com/example/dietideals/domain/models/Links.kt
@@ -0,0 +1,28 @@
+package com.example.dietideals.domain.models
+
+import com.example.dietideals.data.network.serializables.NetLinks
+
+data class Links(
+    val website: String? = null,
+    val instagram: String? = null,
+    val twitter: String? = null,
+    val facebook: String? = null
+) {
+    fun toNetLinks() = NetLinks(
+        website,
+        instagram,
+        twitter,
+        facebook
+    )
+
+    fun toLinksMap(): Map<String, String> {
+        val map = mutableMapOf<String, String>()
+
+        if(website != null) map["website"] = website
+        if(instagram != null) map["instagram"] = instagram
+        if(twitter != null) map["twitter"] = twitter
+        if(facebook != null) map["facebook"] = facebook
+
+        return map
+    }
+}
