108f814a85313a39dbb1c85d5d5cbc7f9af2d7ae
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-10-07 14:04:11 +0200

    Added states for ProfileEditing and Notifications

--- app/src/main/java/com/example/dietideals/data/AppUiState.kt
+++ app/src/main/java/com/example/dietideals/data/AppUiState.kt
@@ -2,26 +2,36 @@ package com.example.dietideals.data
 
 import com.example.dietideals.domain.auxiliary.NewAuction
 import com.example.dietideals.domain.auxiliary.NewUser
+import com.example.dietideals.domain.auxiliary.ProfileForm
 import com.example.dietideals.domain.auxiliary.SearchQuery
+import com.example.dietideals.domain.models.Notification
 import com.example.dietideals.ui.AuctionFetchState
 import com.example.dietideals.ui.HomeFetchState
 import com.example.dietideals.ui.NewAuctionState
+import com.example.dietideals.ui.ProfileEditState
+import com.example.dietideals.ui.ProfileFetchState
 import com.example.dietideals.ui.SearchQueryState
 import com.example.dietideals.ui.SignUpState
 import com.example.dietideals.ui.UserState
+import java.util.SortedSet
 
 data class AppUiState(
     val userState: UserState = UserState.NotLoggedIn(),
     val currentHomeState: HomeFetchState = HomeFetchState.Loading,
     val currentAuctionState: AuctionFetchState = AuctionFetchState.Loading,
+    val otherUserState: ProfileFetchState = ProfileFetchState.Loading,
     val signUpState: SignUpState = SignUpState.Initial(NewUser()),
     val newAuctionState: NewAuctionState = NewAuctionState.Initial(NewAuction()),
     val searchQueryState: SearchQueryState = SearchQueryState.Initial(SearchQuery()),
+    val profileEditState: ProfileEditState = ProfileEditState.Initial(ProfileForm()),
+
+    val notifications: SortedSet<Notification> = sortedSetOf(),
 
     val showSearchDialog: Boolean = false,
     val showNotificationsDialog: Boolean = false,
     val showAllAuctions: Boolean = false,
     val isRefreshing: Boolean = false,
     val isDirectBid: Boolean = false,
-    val isOnline: Boolean = true
+    val isOnline: Boolean = true,
+    val isEditingProfile: Boolean = false
 )
