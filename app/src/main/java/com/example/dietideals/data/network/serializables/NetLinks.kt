9b4aa4cee32f9f732fabe0e1a789f740e489505e
ToytonelloEdu <antonio.ascione15@studenti.unina.it>
2024-10-07 14:06:44 +0200

    Added new classes for Rest endpoints

--- app/src/main/java/com/example/dietideals/data/network/serializables/NetLinks.kt
+++ app/src/main/java/com/example/dietideals/data/network/serializables/NetLinks.kt
@@ -1,19 +1,24 @@
 package com.example.dietideals.data.network.serializables
 
+import android.util.Log
 import com.example.dietideals.domain.models.Links
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class NetLinks(
+    //val user: NetUser? = null,
     val website: String? = null,
     val instagram: String? = null,
     val twitter: String? = null,
     val facebook: String? = null
 ) {
-    fun toLinks() = Links(
-        website,
-        instagram,
-        twitter,
-        facebook
-    )
+    fun toLinks(): Links {
+        Log.d("NetLinks", this.toString())
+        return Links(
+            website,
+            instagram,
+            twitter,
+            facebook
+        )
+    }
 }
